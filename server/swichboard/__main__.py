4698d09688e01d6d06b80131bd172bc0bb05c6d8
chotee <chotee@openended.eu>
2016-10-04 17:00:05 +0200

    Will now send a time message to all registered clients every 10 seconds

--- server/swichboard/__main__.py
+++ server/swichboard/__main__.py
@@ -4,6 +4,11 @@ from twisted.internet.endpoints import TCP4ServerEndpoint
 from swichboard.proto import ProtocolFactory
 from swichboard.switchboard import SwitchboardService
 
+def startServices(objects_to_start):
+    def _call():
+        for obj in objects_to_start:
+            obj.start()
+    return _call
 
 def main():
     from twisted.python import log
@@ -13,6 +18,7 @@ def main():
     from twisted.internet import reactor
     endpoint = TCP4ServerEndpoint(reactor, 8007)
     endpoint.listen(factory)
+    reactor.callWhenRunning(startServices([switchboard]))
     reactor.run()
 
 if __name__ == "__main__":
--- server/swichboard/switchboard.py
+++ server/swichboard/switchboard.py
@@ -1,10 +1,17 @@
 from twisted.python import log
+from twisted.internet import task
+
 
 class SwitchboardService(object):
     def __init__(self):
         log.msg("Create %s" % self.__class__.__name__)
         self._roles = {}
         self._stats = {}
+
+    def start(self):
+        log.msg("Start called on %s" % self.__class__.__name__)
+        loop = task.LoopingCall(self.time_passes)
+        loop.start(10.0)
 
     def got_register(self, client, role):
         client.role = role
@@ -22,3 +29,11 @@ class SwitchboardService(object):
         for src, dst in self._stats:
             summary.append(", ".join([src, dst, str(self._stats[src, dst])]))
         return "\n".join(summary)
+
+    def time_passes(self):
+        clients_list = set()
+        for role_client in self._roles.values():
+            clients_list.update(role_client)
+        for client in clients_list:
+            client.send_message("time's a wastin'")
+        log.msg("Time has passed")
