2da83445d097e2969ee1359b6a9204d4190a9207
chotee <chotee@openended.eu>
2016-10-06 00:42:42 +0200

    Adds calls when connections to senders are lost.

--- server/common/zmq_proto.py
+++ server/common/zmq_proto.py
@@ -1,3 +1,4 @@
+import time
 from enum import Enum
 
 from twisted.internet import reactor
@@ -34,28 +35,72 @@ class ProtocolFactory(Factory):
         log.msg("stopFactory called")
 
 
-class ConnectionStateEnum(Enum):
-    Disconnected = 0
-    Connected = 1
-
 class ConnectionWatchdog(object):
+    """
+    I help keeping watch over the senders connected to a zmq socket. I send them packets at regular intervals.
+    """
     def __init__(self, connection):
+        """
+        Constructor
+        :param connection: The connection of which senders need to be monitored.
+        :type connection: Connection
+        """
         self.connection = connection
         self.beat_frequency = 1.0  # every second
         self.setup_wait = 0.1 # seconds to wait before sending first heartbeat.
-        self.allowed_beat_misses = 3  # How many beats to miss before flagging disconnect.
+        self.allowed_beat_misses = 2  # How many beats to miss before flagging disconnect.
+        self.grace_time = self.beat_frequency * self.allowed_beat_misses
         self.count = 0
+        self.monitors = {}
+        self.actives = set()
 
-    def start_heartbeat(self, remote_identity):
-        def _start(remote_identity):
-            beat = LoopingCall(self.send_beat, remote_identity)
+    def start_monitor(self, remote_identity):
+        """
+        I start the monitoring of a sender with remote_identity.
+        :param remote_identity: The Identity of the sender.
+        :type remote_identity: str
+        """
+        def _start_beats(remote_identity):
+            beat = LoopingCall(self._send_beat, remote_identity)
             beat.start(self.beat_frequency)
-        reactor.callLater(self.setup_wait, _start, remote_identity)
+        reactor.callLater(self.setup_wait, _start_beats, remote_identity)
+        review = LoopingCall(self._review, remote_identity)
+        review.start(self.grace_time)
 
-    def send_beat(self, remote_identity):
-        log.msg("Sending beat %d to remote identity %s" % (self.count, remote_identity))
+    def _send_beat(self, remote_identity):
+        """
+        I send a single beat message to the sender with remote_identity via the self.connection object.
+        :param remote_identity: The remote identity to send a beat to.
+        :type remote_identity: str
+        """
+        #log.msg("Sending beat %d to remote identity %s" % (self.count, remote_identity))
         self.connection.sendMultipart(remote_identity, [b"ping", str(self.count).encode()])
         self.count += 1
+
+    def _review(self, remote_identity):
+        """
+        I periodically review the state of a sender via a connection. If the connection is stale I will call
+        sender_disconnected on self.connection.
+        :param remote_identity: The identity to review.
+        :type remote_identity: str
+        """
+        if remote_identity in self.monitors and self.monitors[remote_identity]+self.grace_time > time.time():
+            return # Activity is good.
+        if remote_identity in self.actives:
+            self.actives.remove(remote_identity)
+            self.connection.sender_disconnected(remote_identity)
+
+    def report_activity(self, remote_identity):
+        """
+        I get called whenever there's activity of a sender.
+        :param remote_identity: ID of the sender that had activity
+        :type remote_identity: str
+        """
+        #log.msg("%s is alive" % remote_identity)
+        self.monitors[remote_identity] = time.time()
+        if remote_identity not in self.actives:
+            self.actives.add(remote_identity)
+            self.connection.sender_established(remote_identity)
 
 
 class Connection(ZmqRouterConnection):
@@ -72,21 +117,44 @@ class Connection(ZmqRouterConnection):
         my_identity = my_identity.encode() if my_identity else None
         remote_identity = remote_identity.encode() if remote_identity else None
         super().__init__(factory, endpoint, my_identity)
-        self.state = ConnectionStateEnum.Disconnected
         self.connecting = (endpoint.type == 'connect')
         self.watchdog = ConnectionWatchdog(self)
+        self.senders = set()
         if self.connecting:
             assert remote_identity, "For connecting, a remote identity is required."
-            self.watchdog.start_heartbeat(remote_identity)
+            self.register_sender(remote_identity)
 
-    def established(self, sender_id):
-        log.msg("connection to %s got established!" % sender_id)
+    def register_sender(self, sender_id):
+        """
+        I register a sender with the system so that the connection will be monitored.
+        :param sender_id: Identity of the sender to monitor
+        :type sender_id: str
+        """
+        self.senders.add(sender_id)
+        self.watchdog.start_monitor(sender_id)
 
-    def disconnected(self, sender_id):
-        log.msg("connection to %s got disconnected!" % sender_id)
+    def sender_established(self, sender_id):
+        """
+        I get called when we can confirm a connection with a sender has been established.
+        :param sender_id: The ID of the sender that we have established
+        :type sender_id: str
+        """
+        log.msg("established connection to %s." % sender_id)
+
+    def sender_disconnected(self, sender_id):
+        """
+        I get called when we have not received any traffic from a sender.
+        :param sender_id: The ID of the sender that we have lost
+        :type sender_id: str
+        """
+        log.msg("lost connection to %s" % sender_id)
 
     def gotMessage(self, sender_id, *frames):
         log.msg("gotMessage %s -> %s" % (sender_id, frames))
+        if sender_id not in self.senders:
+            log.msg("New sender %s" % sender_id)
+            self.register_sender(sender_id)
+        self.watchdog.report_activity(sender_id)
 
 
 class ConnectionFactory(ZmqFactory):
