a294d0f90083b4cd2cb71a1b33a1549612b95287
chotee <chotee@openended.eu>
2016-10-20 00:07:35 +0200

    Working SPI. does nothing yet.

--- refresher/src/main.cpp
+++ refresher/src/main.cpp
@@ -4,92 +4,74 @@
  * Turns on an LED on for one second,
  * then off for one second, repeatedly.
  */
+
+ #include <SPI.h>
 #include "Arduino.h"
+
+#define col_per_elem 8   // Every LED element has 8 columns.
+#define elem_pcb_count 6  // Every PCB has 6 elements
+#define pcb_line_count 3  // There are 3 PCBs in one line
+#define display_line_count 3 // A display as 3 lines
+#define elem_line_count elem_pcb_count*pcb_line_count // Number of elements in a line
+#define elem_display_count elem_line_count*display_line_count // number of elements in the whole display
+#define display_size elem_display_count*col_per_elem // Number of bytes to represent a whole display.
 
 #define BAUD_RATE 115200
 
-// bitmaps * strings * rows * elements
-#define elem_count 18
-#define chain_count 3
-#define chain_size 8*elem_count
-#define bitmap_size chain_count*chain_size
-#define data_size 2*bitmap_size
-uint8_t bitmaps[data_size];
+#define HEARTBEAT_PIN 1
+#define LATCH_PIN 2
 
-bool led_state = 0;
-uint8_t  active_bitmap;
-uint16_t read_counter;
-uint16_t disp_counter;
+uint8_t bitmap[display_size]; //
 
 void setup()
 {
-  for (int16_t i=0; i<data_size; i++) {
-    bitmaps[i] = 0;
+  pinMode(LATCH_PIN, OUTPUT);
+  pinMode(HEARTBEAT_PIN, OUTPUT);
+  SPI.begin();
+  SPI.beginTransaction(SPISettings(15000000, MSBFIRST, SPI_MODE0));
+  // Init the whole bitmap to 0 first.
+  for (int16_t i=0; i<display_size; i++) {
+    bitmap[i] = 0x0;
   }
-  active_bitmap = 0;
-  read_counter  = 0;
-  disp_counter  = 0;
-
-  pinMode(LED_BUILTIN, OUTPUT);  // initialize LED digital pin as an output.
   Serial.begin(BAUD_RATE);  // Setup the serial line to read from
-  Serial.println("Ready");
+  Serial.println("Ready\n");
 }
 
-void toggle_led() {
-    if(led_state) {
-        digitalWrite(LED_BUILTIN, HIGH);
+void toggle_pin(uint8_t pin_nr) {
+    if(digitalRead(pin_nr)) {
+        digitalWrite(pin_nr, LOW);
     } else {
-        digitalWrite(LED_BUILTIN, LOW);
-    }
-    led_state = ~led_state;
-}
-
-void read_serial() {
-    uint8_t bytes_pending = Serial.available();
-    if(bytes_pending) {
-        uint16_t data_start = active_bitmap*bitmap_size + read_counter;
-        Serial.readBytes(bitmaps+data_start, bytes_pending);
+        digitalWrite(pin_nr, HIGH);
     }
 }
 
-void update_row(uint8_t row_nr) {
-    // bits 0, 1, 2 are the data bits. One for each chain.
-    #define enable_bit = 4;
-    #define clock_bit = 5;
-    uint8_t *chain_offsets[chain_count];
-    for(uint8_t c=0; c<chain_count; c++) { // For each chain, where does that data begin?
-        chain_offsets[c] = bitmaps + active_bitmap*bitmap_size + c*chain_size;
-    }
-    uint8_t output_byte = 0;
-    for(uint8_t e=0; e<elem_count; e++) {  // Walk over each byte in the chain
-        output_byte = (output_byte>>chain_count)<<chain_count; // make the first 3 bits zero
-        uint8_t read_byte[chain_count]; // Store the bytes to work with. One for each chain.
-        for(uint8_t c=0; c<chain_count; c++) { // Walk over each of the chains.
-            read_byte[c] = *(chain_offsets[c] + e); // Grab that byte for the chain.
-        }
-
-        for(uint8_t b=0; b<8; b++) { // walk over each of the bits in the byte.
-            for(uint8_t c=0; c<chain_count; c++) { // Walk over each chain
-                // Check if bit b in the byte of that chain is true. if so, set
-                // the bit corresponding with chain c to true. Assumes
-                output_byte |= (read_byte[c] & 1<<b ? 1 : 0) << c;  // set that one bit.
-
-            }
-        }
-    }
+void transfer(uint8_t data) {
+    SPI.transfer(data);
+    Serial.println(data);
 }
 
 void update_display() {
-    for(uint8_t row_counter=0; row_counter<8; row_counter++) {
-        update_row(row_counter);
+    // Iterate over each of the columns
+    for(uint8_t c=0; c < col_per_elem; c++) {
+        // Shift the bytes into the display
+        uint8_t col_select = ~(1<<c);
+        // Shift in one byte for each element in the display
+        for(int16_t e = elem_display_count; e >= 0; --e) {
+            transfer(bitmap[e*col_per_elem+c]);
+            if(e % 6 == 0) { // Add the column selector after each 6 bytes
+                transfer(col_select);
+            }
+        }
+        // After shifting in all data for that column, clock it in with the
+        // LATCH_PIN that's normally kept high.
+        digitalWrite(LATCH_PIN, LOW);
+        digitalWrite(LATCH_PIN, HIGH);
     }
-
 }
 
 void loop()
 {
-    // turn the LED on (HIGH is the voltage level)
-    toggle_led();
-    read_serial();
-    update_display();
+    SPI.transfer(0xC);
+    // update_display();
+    //toggle_pin(HEARTBEAT_PIN);
 }
