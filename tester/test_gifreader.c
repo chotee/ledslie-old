c9681f4976bb562e70abf4f1c56099ef01802c2d
chotee <chotee@openended.eu>
2017-03-04 00:58:21 +0100

    Transform of the bytes.

--- tester/test_gifreader.c
+++ tester/test_gifreader.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <inttypes.h>
 #include "giflib/gif_lib.h"
 
 FILE *fp;
 
+#define DISPLAY_WIDTH 3*6*8
+#define DISPLAY_ROWS 3
+
 int main()
 {
-    char gif_filename[] = "test.gif";
+    char gif_filename[] = "test_2layers.gif";
     GifFileType *gif;
     int *gif_err = 0;
     gif = DGifOpenFileName(gif_filename, gif_err);
@@ -19,16 +23,19 @@ int main()
     }
 
     GifWord width = gif->SavedImages->ImageDesc.Width;
-    char b = 0;
-    for(int j=0; j<8; j++) {
+    GifWord height = gif->SavedImages->ImageDesc.Height;
+    uint8_t display_bytes[DISPLAY_WIDTH*DISPLAY_ROWS];
+    for(int j=0; j<(DISPLAY_WIDTH*DISPLAY_ROWS); j++) {
+        char b = 0;
         unsigned char res = 0;
         for(int i=0; i<8; i++) {
-            int pos = j+(i*width);
-            b = gif->SavedImages->RasterBits[j+(i*width)];
+//            int pos = j+(i*width);
+            b = gif->SavedImages->RasterBits[j+(i*DISPLAY_WIDTH)];
             res = res | (b << i);
-            printf("pos=%d; b=%d; res=%x\n", pos, b, res);
+//            printf("pos=%d; b=%d; res=%x\n", pos, b, res);
         }
-        printf("%02x\n", res, res);
+        display_bytes[j] = res;
+        printf("%02x\n", display_bytes[j]);
     }
     printf("\n");
     return 0;
